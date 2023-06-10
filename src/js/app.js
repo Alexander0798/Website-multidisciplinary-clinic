616520723c90ff032328ffd92d50155573e54fd6
Alexander0798 <rav89515139989@yandex.ru>
2023-06-10 21:25:13 +0300

    сверсталь header navigation about с полным адаптивом

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,12 @@
 import * as flsFunction from './modules/function.js'
 flsFunction.isWebp()
 
+const page = document.querySelector('.page')
+const buttonBurger = page.querySelector('.header__burger')
+const navigate = page.querySelector('.nav')
+const burgerLine = buttonBurger.querySelector('.header__burger-line')
+buttonBurger.addEventListener('click', () => {
+    burgerLine.classList.toggle('header__burger-line_active')
+    navigate.classList.toggle('nav_active')   
+})
 console.log('Hi')
